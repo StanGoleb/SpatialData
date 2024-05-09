57bb18b89949a6df03d7a840e382f7aee980c761
StanGoleb <152734273+StanGoleb@users.noreply.github.com>
2024-05-09 23:56:53 +0200

    Update clusters.py
    Adding additional arguments, comments. Some little changes

--- clusters.py
+++ clusters.py
@@ -8,7 +8,7 @@ import networkx as nx
 from sklearn.neighbors import radius_neighbors_graph
 from sklearn.cluster import AgglomerativeClustering
 
-def clusterize(input_path, chosen_cells, cluster_size):
+def clusterize(input_path, chosen_cells, cluster_num, max_neighbor_distance, component_min_size):
 
     if1_cell_types = ["other", "CD15+Tumor", "CD15-Tumor", "Tcell", "Bcell", "BnTcell", "Neutrophil", "Macrophage", "DC"]
 
@@ -19,9 +19,9 @@ def clusterize(input_path, chosen_cells, cluster_size):
     all_xy = all_df[['nucleus.x', 'nucleus.y']].to_numpy()
     chosen_xy = chosen_df[['nucleus.x', 'nucleus.y']]
 
-    # sąsiedzi, bliżej niż 30
-    all_graph = radius_neighbors_graph(all_xy, 30, mode='distance')
-    chosen_graph = radius_neighbors_graph(chosen_xy, 30, mode='distance')
+    # sąsiedzi, bliżej niż "max_neighbor_distance"
+    all_graph = radius_neighbors_graph(all_xy, max_neighbor_distance, mode='distance')
+    chosen_graph = radius_neighbors_graph(chosen_xy, max_neighbor_distance, mode='distance')
 
     # grafy spójne
     all_G = nx.convert_matrix.from_scipy_sparse_array(all_graph)
@@ -30,15 +30,15 @@ def clusterize(input_path, chosen_cells, cluster_size):
     chosen_G = nx.convert_matrix.from_scipy_sparse_array(chosen_graph)
     chosen_connected_components = list(nx.connected_components(chosen_G))
 
-    # słownik indeksów chosen
+    # słownik indeksów chosen (do mapowania wybranych typów komórek na zbiór wszystkich typów)
     chosen_to_all = {}
     i = 0
     for row_ind, row in chosen_df.iterrows():
         chosen_to_all[i] = row_ind
         i+= 1
     
-    # wybieramy najlepsze grafy spójne
-    chosen_connected_components = [component for component in chosen_connected_components if len(component) > 20]
+    # wybieramy najlepsze grafy spójne - ponad "component_min_size" elementów
+    chosen_connected_components = [component for component in chosen_connected_components if len(component) > component_min_size]
 
     expanded_components_vectors = []
     expanded_components_ids = []
@@ -58,20 +58,21 @@ def clusterize(input_path, chosen_cells, cluster_size):
         component_w_neighbors_df = all_df[all_df['cell.ID'].isin(component_w_neighbors)]
         expanded_components_ids += [component_w_neighbors]
 
-        # tworzymy wektor %-owego uczestnictwa typów komórek w rozszerzonym grafie spójnym
+        # Tworzymy wektor %-owego uczestnictwa typów komórek w rozszerzonym grafie spójnym.
         cell_type_percent_dict = {cell_type: 0 for cell_type in if1_cell_types}
         for cell_ind, cell in component_w_neighbors_df.iterrows():
             cell_type_percent_dict[cell["cell_type"]] += 1
         
         values_sum = sum(cell_type_percent_dict.values())
+        # tworzymy wektory %-owego udziału typów komórek w komponentach.
         for key in cell_type_percent_dict.keys():
             cell_type_percent_dict[key] = cell_type_percent_dict[key]/max(values_sum, 1)
 
         cell_type_vector = list(cell_type_percent_dict.values())
-        cell_type_keys = list(cell_type_percent_dict.keys()).sort()
         expanded_components_vectors += [cell_type_vector]
 
-    hierarchical_cluster = AgglomerativeClustering(n_clusters=cluster_size, metric='euclidean', linkage='complete')
+    # Klastrujemy wektory %-udziału typów komórek w komponentach na "cluster_num" klastrów.
+    hierarchical_cluster = AgglomerativeClustering(n_clusters=cluster_num, metric='euclidean', linkage='complete')
 
     if len(expanded_components_vectors) > 1:
 
@@ -79,6 +80,7 @@ def clusterize(input_path, chosen_cells, cluster_size):
         component_cluster_ids = [i+1 for i in component_clusters]
         
         plot_data = {cluster_id + 1 : [[], []] for cluster_id in range(cluster_size)}
+        # Tworzymy klaster dla tła.
         plot_data[0] = [[], []]
         for cell_id, (x, y) in enumerate(all_xy):
             for component_ind, expanded_component in enumerate(expanded_components_ids):
@@ -93,23 +95,5 @@ def clusterize(input_path, chosen_cells, cluster_size):
     
     else:
         plot_data = {0: [[all_xy[:][0]],[all_xy[:][1]]]}
-
-                
+ 
     return plot_data, expanded_components_vectors, component_clusters
-
-
-# overview danych - wielu pacjentów, jaki jest rozkłąd % zawartości b-cell i t-cell wśród pacjentów. 
-# ogólna funkcja sklrean radius neighbors graph która zbuduje graf na tych komórkach dla dowolnego fenotypu (na przykład tylko CK i coś innego)
-# zbudować graf sąsiedztwa dla wszystkich komórek, obok graf dla komórek typu B (np. CD20)
-#   w dataframe wybieram tylko B-celle (CD20+).
-#   najpierw graf sąsiedztwa z parametrem jak blisko mają być na wszystkim i potem drugi zbudowany tylko na punktach z CD20+
-    # elementy CD20+ blisko siebie są najprawdopodobniej TLS
-#   przejść przez komórki CD20+ funckją connected components. szukamy sąsiadów całego spójnego grafu CD20+
-
-# taki graf spójny razem z sasiadami będzie miał wektor [20% tcelli, 50%bcelli,...]. takie wektory chcemy sklastrować np. na średnio zawsze 60% bcelli. potem zaznaczamy je w obrębie raka
-
-
-# na tym zrobić histogram  z zaznaczonymi TLS
-
-#github
-# raport
